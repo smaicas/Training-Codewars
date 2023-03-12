ca3aff801200284a6cbc5f7630c26c5ae4ad0be9
smaicas <samuel.maicas.development@gmail.com>
2023-03-12 21:11:09 +0100

    Add SoManyPermutations, SumOfIntervals

---
+++ Kyu4/SoManyPermutations/Program.cs
@@ -0,0 +1,38 @@
+﻿//In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+
+//    Create as many "shufflings" as you can!
+
+//Examples:
+
+//With input 'a':
+//Your function should return: ['a']
+
+//With input 'ab':
+//Your function should return ['ab', 'ba']
+
+//With input 'abc':
+//Your function should return ['abc','acb','bac','bca','cab','cba']
+
+//With input 'aabb':
+//Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+//Note: The order of the permutations doesn't matter.
+
+//    Good luck!
+
+Console.WriteLine(string.Join(",", Permutations("aabb")));
+
+Console.ReadKey();
+
+static List<string> Permutations(string s)
+{
+    if (s.Length == 1)
+    {
+        return new[] { s }.ToList();
+    }
+    else
+    {
+        return Permutations(s[1..]).SelectMany(permutation =>
+            Enumerable.Range(0, s.Length).Select(index =>
+                permutation.Insert(index, s[0].ToString()))).GroupBy(x => x).Select(x => x.First()).ToList();
+    }
+}
---
+++ Kyu4/SumOfIntervals/Program.cs
@@ -0,0 +1,60 @@
+﻿//Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+//    Intervals
+//    Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+//    Overlapping Intervals
+//    List containing overlapping intervals:
+
+//[
+//[1, 4],
+//[7, 10],
+//[3, 5]
+//    ]
+//The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
+
+//    Examples:
+//sumIntervals( [
+//    [1, 2],
+//[6, 10],
+//[11, 15]
+//    ] ) => 9
+
+//sumIntervals( [
+//    [1, 4],
+//[7, 10],
+//[3, 5]
+//    ] ) => 7
+
+//sumIntervals( [
+//    [1, 5],
+//[10, 20],
+//[1, 6],
+//[16, 19],
+//[5, 11]
+//    ] ) => 19
+
+//sumIntervals( [
+//    [0, 20],
+//[-100000000, 10],
+//[30, 40]
+//    ] ) => 100000030
+//Tests with large intervals
+//Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
+
+//(int, int)[] Param = new (int, int)[] { (1, 2), (6, 10), (11, 15) };
+(int, int)[] Param = new (int, int)[] { (1, 5), (10, 20), (1, 6), (16, 19), (5, 11) };
+Console.WriteLine(Intervals.SumIntervals(Param.OrderBy(x => x.Item1).ToArray()));
+
+public class Intervals
+{
+    public static int SumIntervals((int, int)[] intervals) => SumRec(intervals.OrderBy(x => x.Item1).ThenBy(x => x.Item2).ToArray(), Int32.MinValue);
+
+    private static int SumRec(IList<(int, int)> intervals, int max)
+    {
+        if (intervals.Count <= 0) return 0;
+        int val = intervals[0].Item2 - Math.Max(max, intervals[0].Item1) > 0 ? intervals[0].Item2 - Math.Max(max, intervals[0].Item1) : 0;
+        if (intervals.Count == 1) return val;
+        return val + SumRec(intervals.Skip(1).ToArray(), Math.Max(max, intervals[0].Item2));
+    }
+}
